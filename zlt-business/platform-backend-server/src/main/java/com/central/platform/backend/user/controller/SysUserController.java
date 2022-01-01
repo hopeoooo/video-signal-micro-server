0f41022d2acf182150363de466e2e6da74637cd6
Gaarajs <gaarajs1688>
2022-01-01 18:10:12 +0700

    服务端之间的调用，引用的是feign的接口

--- zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/user/controller/SysUserController.java
+++ zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/user/controller/SysUserController.java
@@ -1,44 +1,32 @@
 package com.central.platform.backend.user.controller;
 
-import com.central.common.model.PageResult;
-import com.central.common.model.SysUser;
-import com.central.user.service.ISysUserService;
+import com.central.common.feign.UserService;
+import com.central.common.model.LoginAppUser;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.annotation.Resource;
 
 @RestController
-@Api(tags = "会员中心")
+@Api(tags = "test1")
 @Slf4j
-@RequestMapping("/sysUser")
+@RequestMapping("/test")
 public class SysUserController {
 
-
-    @Autowired
-    private ISysUserService appUserService;
+    @Resource
+    UserService userService;
 
     /**
-     * 用户查询
-     *
-     * @param params
-     * @return
+     * 测试
      */
-    @ApiOperation(value = "用户查询列表")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "page", value = "分页起始位置", required = true, dataType = "Integer"),
-            @ApiImplicitParam(name = "limit", value = "分页结束位置", required = true, dataType = "Integer")
-    })
-    @GetMapping("/users")
-    public PageResult<SysUser> findUsers(@RequestParam Map<String, Object> params) {
-        return appUserService.findUsers(params);
+    @ApiOperation(value = "测试")
+    @GetMapping("/test")
+    public String test() {
+        LoginAppUser loginAppUser =  userService.findByMobile("admin");
+        return loginAppUser.getUserId();
     }
 }
