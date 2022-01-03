b792ce367d7733171065081ee30c1f5419f21ae8
“tony117269” <tony1172692857@gmail.com>
2022-01-03 14:07:45 +0800

    会员中心查询日志

---
+++ zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/user/controller/SysUserLogController.java
@@ -0,0 +1,13 @@
+package com.central.platform.backend.user.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "会员中心日志")
+@Slf4j
+@RequestMapping("/sysUserLog")
+public class SysUserLogController {
+}
