a4e365d69bf6af83b9cf6b842d8cbebd41b571d1
Gaarajs <gaarajs1688>
2022-01-03 13:06:18 +0700

    修复：后台管理启动失败,feign报错

--- zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/PlatformBackendApp.java
+++ zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/PlatformBackendApp.java
@@ -3,10 +3,12 @@ package com.central.platform.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * app后端管理系统服
  */
+@EnableFeignClients(basePackages = "com.central.common")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PlatformBackendApp {
