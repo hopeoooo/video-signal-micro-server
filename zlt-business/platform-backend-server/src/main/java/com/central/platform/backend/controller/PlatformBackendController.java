184098c9aa39087bdf5b99d9ecd9e4c9700b0bf9
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-01-08 18:27:46 +0700

    添加接口注释

--- zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/controller/PlatformBackendController.java
+++ zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/controller/PlatformBackendController.java
@@ -1,5 +1,6 @@
 package com.central.platform.backend.controller;
 
+import com.central.common.model.SysBanner;
 import com.central.config.dto.TouristDto;
 import com.central.config.feign.ConfigService;
 import com.central.common.model.Result;
@@ -93,5 +94,54 @@ public class PlatformBackendController {
     }
 
 
+    /**
+     * 查询banner列表
+     */
+    @ApiOperation("查询banner列表")
+    @ResponseBody
+    @GetMapping("/banner/findBannerList")
+    public Result findBannerList() {
+        return configService.findBannerList();
+
+    }
+
+    /**
+     * 删除
+     *
+     * @param id
+     */
+    @ApiOperation("删除banner")
+    @DeleteMapping(value = "/banner/delBannerId/{id}")
+    public Result delBannerId(@PathVariable Long id) {
+        return configService.delBannerId(id);
+    }
+
+    /**
+     * 修改banner状态
+     *
+     * @param params
+     * @return
+     */
+    @ApiOperation(value = "修改banner状态")
+    @GetMapping("/banner/updateState")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "id", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "state", value = "状态", required = true, dataType = "Boolean")
+    })
+    public Result updateState(@RequestParam Map<String, Object> params) {
+        return configService.updateState(params);
+    }
+
+    /**
+     * 新增or更新
+     *
+     * @param sysBanner
+     * @return
+     */
+    @ApiOperation(value = "新增or更新banner")
+    @PostMapping("/banner/saveOrUpdate")
+    public Result saveOrUpdate(@RequestBody SysBanner sysBanner) throws Exception {
+        return configService.saveOrUpdate(sysBanner);
+    }
 
 }
