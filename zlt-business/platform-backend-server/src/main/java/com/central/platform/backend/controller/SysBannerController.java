5f8e6cf813c155791a7a465097609b56bf56ac50
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-01-12 19:18:24 +0700

    公告和轮播图限制最多添加20条数据

--- zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/controller/SysBannerController.java
+++ zlt-business/platform-backend-server/src/main/java/com/central/platform/backend/controller/SysBannerController.java
@@ -75,21 +75,14 @@ public class SysBannerController {
     @ApiOperation(value = "新增or更新banner")
     @PostMapping(value = "/banner/saveOrUpdate",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "startTime", value = "起始时间查询", required = false),
-            @ApiImplicitParam(name = "endTime", value = "结束时间查询", required = false),
-            @ApiImplicitParam(name = "startMode", value = "开始方式(0:即时,1:定时)", required = true),
-            @ApiImplicitParam(name = "endMode", value = "结束方式(0:长期,1:到期)", required = true),
             @ApiImplicitParam(name = "linkUrl", value = "链接url", required = false),
             @ApiImplicitParam(name = "sort", value = "排序", required = true),
             @ApiImplicitParam(name = "id", value = "id", required = false),
     })
     public Result saveOrUpdate(
             @RequestPart(value = "fileH5", required = false) MultipartFile fileH5,
-            @RequestPart(value = "fileWeb", required = false) MultipartFile fileWeb,Integer sort,String linkUrl,
-            @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") String startTime,
-            @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") String endTime,
-            Integer startMode,Integer endMode, Long id
+            @RequestPart(value = "fileWeb", required = false) MultipartFile fileWeb,Integer sort,String linkUrl,Long id
     ) throws Exception {
-        return configService.saveOrUpdate(fileH5,fileWeb,sort,linkUrl,startTime,endTime,startMode,endMode,id);
+        return configService.saveOrUpdate(fileH5,fileWeb,sort,linkUrl,id);
     }
 }
