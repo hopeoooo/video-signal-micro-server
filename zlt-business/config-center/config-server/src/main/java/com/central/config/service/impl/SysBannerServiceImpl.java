6f04c46b1b85e1131d6a53acbae9c3911cc2456f
benson <bb0491742@gmail.com>
2022-01-13 20:13:14 +0800

    配置服务-轮播图数据发生变化时异步推送到前台

--- zlt-business/config-center/config-server/src/main/java/com/central/config/service/impl/SysBannerServiceImpl.java
+++ zlt-business/config-center/config-server/src/main/java/com/central/config/service/impl/SysBannerServiceImpl.java
@@ -1,14 +1,22 @@
 package com.central.config.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.central.common.model.PushResult;
+import com.central.common.model.Result;
 import com.central.common.service.impl.SuperServiceImpl;
 import com.central.config.mapper.SysBannerMapper;
 import com.central.config.model.SysBanner;
+import com.central.config.model.SysNotice;
 import com.central.config.service.ISysBannerService;
+import com.central.push.feign.PushService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -20,6 +28,9 @@ import java.util.Map;
 @Service
 @CacheConfig(cacheNames = {"sysBanner"})
 public class SysBannerServiceImpl extends SuperServiceImpl<SysBannerMapper, SysBanner> implements ISysBannerService {
+
+    @Autowired
+    private PushService pushService;
 
     @Override
     public List<SysBanner> findBannerList() {
@@ -70,5 +81,23 @@ public class SysBannerServiceImpl extends SuperServiceImpl<SysBannerMapper, SysB
     @Override
     public Integer queryTotal(Integer sort) {
         return baseMapper.queryTotal(sort);
+    }
+
+    @Override
+    public List<SysBanner> getBannerList() {
+        LambdaQueryWrapper<SysBanner> lqw = Wrappers.lambdaQuery();
+        lqw.eq(SysBanner::getState, Boolean.TRUE);
+        lqw.orderByAsc(SysBanner::getSort);
+        List<SysBanner> bannerList = baseMapper.selectList(lqw);
+        return bannerList;
+    }
+
+    @Override
+    @Async
+    public void syncPushBannerToWebApp() {
+        List<SysBanner> bannerList = getBannerList();
+        PushResult<List<SysBanner>> pushResult = PushResult.succeed(bannerList, "banner");
+        Result<String> push = pushService.push(JSONObject.toJSONString(pushResult));
+        log.info("轮播图推送结果:{}",push);
     }
 }
