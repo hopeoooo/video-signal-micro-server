b2920d615f0306cce59d0d441af0b13d0fe0435b
benson <bb0491742@gmail.com>
2022-01-13 14:27:57 +0800

    新建消息推送服务模块

---
+++ zlt-business/push-center/push-server/src/main/java/com/central/push/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.central.push.config;
+
+import com.central.common.resolver.ClientArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * Token参数解析
+     *
+     * @param argumentResolvers 解析类
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        //注入应用信息
+        argumentResolvers.add(new ClientArgumentResolver());
+    }
+}
