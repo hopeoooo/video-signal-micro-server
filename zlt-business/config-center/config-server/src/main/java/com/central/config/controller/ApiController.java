5f92d9a724e11894414ac15206a9c0e23867962a
benson <bb0491742@gmail.com>
2022-01-13 17:02:22 +0800

    推送服务-代码补漏

--- zlt-business/config-center/config-server/src/main/java/com/central/config/controller/ApiController.java
+++ zlt-business/config-center/config-server/src/main/java/com/central/config/controller/ApiController.java
@@ -8,6 +8,7 @@ import com.central.config.model.DownloadStation;
 import com.central.config.model.SysPlatformConfig;
 import com.central.config.service.IDownloadStationService;
 import com.central.config.service.ISysPlatformConfigService;
+import com.central.push.feign.PushService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -32,8 +33,8 @@ public class ApiController {
     private IDownloadStationService downloadStationService;
     @Autowired
     private ISysPlatformConfigService platformConfigService;
-//    @Autowired
-//    private PushService pushService;
+    @Autowired
+    private PushService pushService;
 
     @Value("${zlt.app.version:1.0.0}")
     private String version;
@@ -44,12 +45,12 @@ public class ApiController {
         return Result.succeed(version, "查询成功");
     }
 
-//    @ApiOperation(value = "消息推送测试")
-//    @GetMapping("/push/{message}")
-//    public Result<String> push(@PathVariable(name = "message") String message) {
-//        pushService.push(message);
-//        return Result.succeed();
-//    }
+    @ApiOperation(value = "消息推送测试")
+    @GetMapping("/push/{message}")
+    public Result<String> push(@PathVariable(name = "message") String message) {
+        pushService.push(message);
+        return Result.succeed();
+    }
 
     @ApiOperation("logo查询")
     @GetMapping("/findLogoUrlInfo")
