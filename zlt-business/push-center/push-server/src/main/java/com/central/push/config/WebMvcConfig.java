1ada29d77b37b278620db8816f2177585c0e0e6a
benson <bb0491742@gmail.com>
2022-01-13 22:49:18 +0800

    推送服务-bug修复

--- zlt-business/push-center/push-server/src/main/java/com/central/push/config/WebMvcConfig.java
+++ zlt-business/push-center/push-server/src/main/java/com/central/push/config/WebMvcConfig.java
@@ -1,23 +1,8 @@
 package com.central.push.config;
 
-import com.central.common.resolver.ClientArgumentResolver;
+import com.central.common.config.DefaultWebMvcConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    /**
-     * Token参数解析
-     *
-     * @param argumentResolvers 解析类
-     */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        //注入应用信息
-        argumentResolvers.add(new ClientArgumentResolver());
-    }
+public class WebMvcConfig extends DefaultWebMvcConfig {
 }
