b1d5186932569d5ab479273c6d3227de3e608304
benson <bb0491742@gmail.com>
2022-01-14 13:50:47 +0800

    推送服务-移除数据库配置

--- zlt-business/push-center/push-server/src/main/java/com/central/push/PushCenterApp.java
+++ zlt-business/push-center/push-server/src/main/java/com/central/push/PushCenterApp.java
@@ -3,6 +3,7 @@ package com.central.push;
 import com.central.common.lb.annotation.EnableFeignInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -12,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignInterceptor
 @EnableFeignClients(basePackages = "com.central")
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PushCenterApp {
     public static void main(String[] args) {
         SpringApplication.run(PushCenterApp.class, args);
