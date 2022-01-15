b864ddafc9686c6a5e8902c53a192f7c32fd80c9
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-01-15 12:05:04 +0700

    app下载配置修改

--- zlt-business/config-center/config-server/src/main/java/com/central/config/service/IDownloadStationService.java
+++ zlt-business/config-center/config-server/src/main/java/com/central/config/service/IDownloadStationService.java
@@ -1,15 +1,17 @@
 package com.central.config.service;
 
+import com.central.common.model.PageResult2;
 import com.central.common.model.Result;
 import com.central.common.service.ISuperService;
 import com.central.config.model.DownloadStation;
 
 import java.util.List;
+import java.util.Map;
 
 
 public interface IDownloadStationService extends ISuperService<DownloadStation> {
 
-     List<DownloadStation> findDownloadStationList() ;
+     PageResult2<DownloadStation> findDownloadStationList(Map<String, Object> map) ;
 
      Result saveOrUpdateDownloadStation(DownloadStation downloadStation) throws Exception;
 }
