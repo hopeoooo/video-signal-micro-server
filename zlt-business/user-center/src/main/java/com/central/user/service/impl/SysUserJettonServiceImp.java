0eab74e51724a9a1727b05bb08a38da064439089
kate <kate98765@protonmail.com>
2022-01-17 16:27:47 +0800

    增加筹码设置功能

---
+++ zlt-business/user-center/src/main/java/com/central/user/service/impl/SysUserJettonServiceImp.java
@@ -0,0 +1,14 @@
+package com.central.user.service.impl;
+
+import com.central.common.model.SysUserJetton;
+import com.central.common.service.impl.SuperServiceImpl;
+import com.central.user.mapper.SysUserJettonMapper;
+import com.central.user.service.ISysUserJettonService;
+import com.central.user.service.ISysUserJettonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SysUserJettonServiceImp extends SuperServiceImpl<SysUserJettonMapper, SysUserJetton> implements ISysUserJettonService {
+}
