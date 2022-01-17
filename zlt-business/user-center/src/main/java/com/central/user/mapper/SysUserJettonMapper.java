3b2a59939b794c6977d26355caee2d465eb6b854
kate <kate98765@protonmail.com>
2022-01-17 16:27:47 +0800

    增加筹码设置功能

---
+++ zlt-business/user-center/src/main/java/com/central/user/mapper/SysUserJettonMapper.java
@@ -0,0 +1,9 @@
+package com.central.user.mapper;
+
+import com.central.common.model.SysUserJetton;
+import com.central.db.mapper.SuperMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysUserJettonMapper extends SuperMapper<SysUserJetton> {
+}
