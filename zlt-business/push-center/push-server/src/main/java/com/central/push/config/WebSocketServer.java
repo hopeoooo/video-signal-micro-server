880ec8c7eafa2dfded91681b6fc60ec4f3a9860d
benson <bb0491742@gmail.com>
2022-01-19 19:49:40 +0800

    用户服务-登录用户修改登录密码接口

--- zlt-business/push-center/push-server/src/main/java/com/central/push/config/WebSocketServer.java
+++ zlt-business/push-center/push-server/src/main/java/com/central/push/config/WebSocketServer.java
@@ -1,6 +1,8 @@
 package com.central.push.config;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.central.common.model.PushResult;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -55,7 +58,8 @@ public class WebSocketServer {
     @OnMessage
     public void onMessage(String message, Session session) {
         log.info("来自客户端的消息：{}", message);
-        SendMessage(session, "收到消息，消息内容：" + message);
+        PushResult pushResult = PushResult.succeed(message, "heartbeat");
+        SendMessage(session, JSONObject.toJSONString(pushResult));
 
     }
 
