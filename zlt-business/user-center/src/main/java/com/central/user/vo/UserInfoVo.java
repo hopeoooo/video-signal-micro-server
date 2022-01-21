b250feea12ea8a41770c74c5e5221c880e4ac048
benson <bb0491742@gmail.com>
2022-01-21 13:38:28 +0800

    用户服务-用户基本信息接口新增账号类型字段

--- zlt-business/user-center/src/main/java/com/central/user/vo/UserInfoVo.java
+++ zlt-business/user-center/src/main/java/com/central/user/vo/UserInfoVo.java
@@ -14,4 +14,6 @@ public class UserInfoVo {
     private String headImgUrl;
     @ApiModelProperty(value = "投注自动提交 开启：true,关闭：false")
     private Boolean isAutoBet;
+    @ApiModelProperty(value = "账号类型：APP：前端app用户，APP_GUEST：前端app游客用户，BACKEND：后端管理用户")
+    private String type;
 }
