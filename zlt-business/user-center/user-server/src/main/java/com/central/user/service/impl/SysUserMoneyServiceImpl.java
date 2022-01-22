2f572a2df2fdd92e8ab3a6a45dad90a29f1d0798
benson <bb0491742@gmail.com>
2022-01-22 15:28:30 +0800

    用户服务-用户钱包推送规则修改

--- zlt-business/user-center/user-server/src/main/java/com/central/user/service/impl/SysUserMoneyServiceImpl.java
+++ zlt-business/user-center/user-server/src/main/java/com/central/user/service/impl/SysUserMoneyServiceImpl.java
@@ -11,9 +11,11 @@ import com.central.push.feign.PushService;
 import com.central.user.mapper.SysUserMoneyMapper;
 import com.central.user.service.ISysTansterMoneyLogService;
 import com.central.user.service.ISysUserMoneyService;
+import com.central.user.vo.SysUserMoneyVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
@@ -92,9 +94,14 @@ public class SysUserMoneyServiceImpl extends SuperServiceImpl<SysUserMoneyMapper
     @Async
     public void syncPushMoneyToWebApp(Long userId) {
         SysUserMoney money = findByUserId(userId);
-        PushResult<SysUserMoney> pushResult = PushResult.succeed(money, "money","用户钱包推送成功");
+        if (money == null) {
+            money = new SysUserMoney();
+        }
+        SysUserMoneyVo vo = new SysUserMoneyVo();
+        BeanUtils.copyProperties(money, vo);
+        PushResult<SysUserMoneyVo> pushResult = PushResult.succeed(vo, "money","用户钱包推送成功");
         Result<String> push = pushService.sendOneMessage(userId.toString(),JSONObject.toJSONString(pushResult));
-        log.info("用户金额userId:{},推送结果:{}", userId, push);
+        log.info("用户钱包userId:{},推送结果:{}", userId, push);
     }
 
     @Override
