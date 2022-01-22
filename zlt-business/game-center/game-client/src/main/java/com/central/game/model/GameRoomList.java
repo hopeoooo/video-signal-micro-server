47ea7e77ed3b703074c8d53c0c880b3048c4e4c3
benson <bb0491742@gmail.com>
2022-01-22 14:55:55 +0800

    游戏服务-新增房间列表接口

---
+++ zlt-business/game-center/game-client/src/main/java/com/central/game/model/GameRoomList.java
@@ -0,0 +1,49 @@
+package com.central.game.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.central.common.model.SuperEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = false)
+@TableName("game_room_list")
+@ApiModel("房间列表")
+public class GameRoomList extends SuperEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "游戏表id")
+    private Long gameId;
+
+    @ApiModelProperty(value = "游戏房间名称")
+    private String gameRoomName;
+
+    @ApiModelProperty(value = "游戏房间状态 0禁用，1：正常，2：维护")
+    private Integer roomStatus;
+
+    @ApiModelProperty(value = "返佣类型1：免佣，2：抽佣")
+    private Integer commissionType;
+
+    @ApiModelProperty(value = "限红单局下注最小金额")
+    private BigDecimal betMin;
+
+    @ApiModelProperty(value = "限红单局总下注最大金额")
+    private BigDecimal betMax;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ApiModelProperty(value = "维护开始时间")
+    private Date maintainStart;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ApiModelProperty(value = "维护结束时间")
+    private Date maintainEnd;
+}
---
+++ zlt-business/game-center/game-server/src/main/java/com/central/game/controller/GameRoomListController.java
@@ -0,0 +1,86 @@
+package com.central.game.controller;
+
+import com.central.common.model.Result;
+import com.central.game.model.GameRoomList;
+import com.central.game.service.IGameRoomListService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/gameRoomList")
+@Api(tags = "游戏房间列表列表")
+public class GameRoomListController {
+
+    @Autowired
+    private IGameRoomListService iGameRoomListService;
+
+    /**
+     * 分页查询房间数据
+     *
+     * @param gameId
+     * @return
+     */
+    @ResponseBody
+    @ApiOperation(value = "查询房间列表数据")
+    @GetMapping("/findList")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "gameId", value = "游戏Id", required = false, dataType = "Long")
+    })
+    public Result<List<GameRoomList>> findList(@RequestParam("gameId") Long gameId) {
+        List<GameRoomList> gameRoomList = iGameRoomListService.findGameRoomList(gameId);
+        return Result.succeed(gameRoomList);
+    }
+
+    @ApiOperation(value = "根据游戏ID查询房间列表(前台用)")
+    @GetMapping("/findRoomListByGameId/{gameId}")
+    public Result<List<GameRoomList>> findRoomListByGameId(@PathVariable("gameId") Long gameId) {
+        List<GameRoomList> gameRoomList = iGameRoomListService.lambdaQuery().eq(GameRoomList::getGameId,gameId).ne(GameRoomList::getRoomStatus,0).list();
+        return Result.succeed(gameRoomList);
+    }
+
+    @ApiOperation(value = "根据房间ID查询房间详情")
+    @GetMapping("/findRoomDetailById/{id}")
+    public Result<GameRoomList> findRoomDetailById(@PathVariable("id") Long id) {
+        GameRoomList room = iGameRoomListService.getById(id);
+        return Result.succeed(room);
+    }
+
+    @ApiOperation(value = "新增/更新")
+    @PostMapping("/save")
+    public Result save(@RequestBody GameRoomList gameRoomList) {
+        iGameRoomListService.saveOrUpdate(gameRoomList);
+        return Result.succeed();
+    }
+
+    @ApiOperation(value = "根据ID修改房间状态")
+    @PostMapping("/roomStatus/{id}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "roomStatus", value = "游戏房间状态 0禁用，1：正常，2：维护", required = false, dataType = "Integer")
+    })
+    public Result updateRoomStatus(@PathVariable Long id, @RequestParam("roomStatus") Integer roomStatus) {
+        if(roomStatus > 2 || roomStatus < 0){
+            return Result.failed("参数不合法");
+        }
+        Boolean result = iGameRoomListService.updateRoomStatus(id, roomStatus);
+        if(result){
+            return Result.succeed();
+        }else{
+            return Result.failed("修改房间状态失败");
+        }
+    }
+
+    @ApiOperation(value = "根据ID删除")
+    @DeleteMapping("/deleteById/{id}")
+    public Result roomDeleteById(@PathVariable Long id) {
+        iGameRoomListService.removeById(id);
+        return Result.succeed();
+    }
+}
