1517d9f048bf89b12f1e606864837dd7c5a99419
kygo <pp5808574@gmail.com>
2022-02-18 17:21:47 +0700

    基础配置。

---
+++ platform-app/app-member-management/src/main/java/com/central/app/AppMemberManagementApp.java
@@ -0,0 +1,35 @@
+package com.central.app;
+
+import com.central.common.lb.annotation.EnableFeignInterceptor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * App管理系统服-会员管理
+ */
+@EnableFeignInterceptor
+@EnableFeignClients(basePackages = "com.central")
+@EnableDiscoveryClient
+@EnableScheduling
+@SpringBootApplication
+@ComponentScans(value = {
+        @ComponentScan(value = "com.central")
+})
+public class AppMemberManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMemberManagementApp.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
