3c53fbfa8ea549a197bea236b930a773f1a84c32
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-03-21 14:01:43 +0700

    总投注记录优化

--- zlt-business/game-center/game-client/src/main/java/com/central/game/feign/GameService.java
+++ zlt-business/game-center/game-client/src/main/java/com/central/game/feign/GameService.java
@@ -7,11 +7,14 @@ import com.central.game.dto.GameRecordDto;
 import com.central.game.dto.GameRecordReportDto;
 import com.central.game.model.GameRecord;
 import com.central.game.model.co.GameListCo;
+import com.central.game.model.co.GameRecordBetCo;
+import com.central.game.model.co.GameRecordBetTotalCo;
 import com.central.game.model.co.GameRoomListCo;
 import com.central.game.feign.callback.GameServiceFallbackFactory;
 import com.central.game.model.GameList;
 import com.central.game.model.GameRoomList;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -116,10 +119,10 @@ public interface GameService {
      * @return
      */
     @GetMapping("/gameRecord/findList")
-     Result<PageResult<GameRecord>> findList(@RequestParam Map<String, Object> params) ;
+     Result<PageResult<GameRecord>> findList(@SpringQueryMap GameRecordBetCo params ) ;
 
     @GetMapping("/gameRecord/findGameRecordTotal")
-    Result<GameRecordDto> findGameRecordTotal(@RequestParam Map<String, Object> params) ;
+    Result<GameRecordDto> findGameRecordTotal(@SpringQueryMap GameRecordBetTotalCo params) ;
 
     @GetMapping("/gameRecord/findBetAmountTotal")
     Result<GameRecordReportDto> findBetAmountTotal(@RequestParam Map<String, Object> params) ;
