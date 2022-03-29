8edb9f0b85a50953a93fed8cc9cd8eed23ab0545
benson <bb0491742@gmail.com>
2022-03-29 16:18:08 +0800

    查询本局即时彩池接口

--- zlt-business/game-center/game-client/src/main/java/com/central/game/model/co/GameRecordLivePotCo.java
+++ zlt-business/game-center/game-client/src/main/java/com/central/game/model/co/GameRecordLivePotCo.java
@@ -4,34 +4,25 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import java.math.BigDecimal;
-import java.util.List;
 
 @Data
-@ApiModel("下注结果入参")
-public class GameRecordCo {
+@ApiModel("即时彩池入参")
+public class GameRecordLivePotCo {
 
     @ApiModelProperty(value = "游戏ID", required = true)
     @NotBlank(message = "游戏ID不能为空")
     private Long gameId;
 
-    @ApiModelProperty(value = "桌台编号", required = true)
+    @ApiModelProperty(value = "桌台编号")
     @NotBlank(message = "桌台编号不能为空")
     private String tableNum;
 
-    @ApiModelProperty(value = "靴号", required = true)
+    @ApiModelProperty(value = "靴号")
     @NotBlank(message = "靴号不能为空")
     private String bootNum;
 
-    @ApiModelProperty(value = "局号", required = true)
+    @ApiModelProperty(value = "局号")
     @NotBlank(message = "局号不能为空")
     private String bureauNum;
-
-    @ApiModelProperty(value = "下注结果集", required = true)
-    @NotEmpty(message = "下注结果集不能为空")
-    @Valid
-    private List<GameRecordBetDataCo> betResult;
 }
