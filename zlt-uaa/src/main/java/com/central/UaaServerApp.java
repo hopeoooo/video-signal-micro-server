38b27f5e85664571b197bfecc88c0ca9baf9d7e2
benson <bb0491742@gmail.com>
2022-06-15 18:40:44 +0800

    单点登录调试

--- zlt-uaa/src/main/java/com/central/UaaServerApp.java
+++ zlt-uaa/src/main/java/com/central/UaaServerApp.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -18,6 +20,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //@EnableRedisHttpSession
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan("com.central")
 public class UaaServerApp {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(UaaServerApp.class);
